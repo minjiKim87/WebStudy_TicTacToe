5b728d178eed747a38b4a77deb0629c52ed6987d
minjiKim87 <68892132+minjiKim87@users.noreply.github.com>
2023-01-04 19:04:19 +0900

    0104

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,6 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  const selectedPlayerId = event.target.dataset.playerid;
+  editPlayer = +event.target.dataset.playerid;
   playerConfigOverlayElement.style.display = "block";
   backdropElement.style.display = "block";
 }
@@ -6,11 +8,28 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = "";
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const enteredPlayername = formData.get("playername");
-  console.log(enteredPlayername);
+  const enteredPlayername = formData.get("playername").trim();
+
+  if (!enteredPlayername) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name";
+    return;
+  }
+
+  const updatePlayerDataElement = document.getElementById(
+    "player-" + editPlayer + "-data"
+  );
+  updatePlayerDataElement.children[1].textContent = enteredPlayername;
+
+  players[editedPlayer - 1].name = enteredPlayername;
+
+  closePlayerConfig();
 }
